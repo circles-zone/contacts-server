0bd876b47c66479268d716d793a2403e25db9652
Tal Rosenberger <tal@circles.zone>
2026-03-19 07:10:45 +0200

    Add TODO for accessing all columns in contact tables
    Added a TODO comment to indicate future enhancements for accessing all columns in related tables.

--- contacts-server/src/graphql/schema.ts
+++ contacts-server/src/graphql/schema.ts
@@ -1,5 +1,7 @@
 import { gql } from "graphql-tag";
 
+// TODO we want to be able to access all columns in the original contact_table and tables which are linked tomit using foreignn keys i.e. contac__email_address_table, email_adresss_table, contact__text_block_table, text_block_table ...
+
 export const typeDefs = gql`
   type Contact {
     id: ID!
